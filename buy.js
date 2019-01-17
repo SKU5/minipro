9088fd2406335887df9a2a0d3da99f24fc7cc651
酒井由吾 <sakaiyugo@sakaiyuwarenoMacBook-ea.local>
2019-01-17 13:41:29 +0900

    final

--- buy.js
+++ buy.js
@@ -1,4 +1,22 @@
-function submit(){
-  var name = document.getElementById('name').value;
-  alert(name + '様' + 'ご購入ありがとうございます');
+function submit() {
+        var name = document.getElementById("name").value
+      	var arr_checkBoxes = document.getElementsByClassName("checkBoxes");
+      	var count = 0;
+      	for (var i = 0; i < arr_checkBoxes.length; i++) {
+      		if (arr_checkBoxes[i].checked) {
+      			count++;
+      		}
+        }
+      	if (count > 0) {
+          alert(name + '様' + 'ご購入ありがとうございます');
+      		return true;
+      	} else {
+      		alert("項目を1つ以上選択してください。");
+      		return false;
+      	}
+      }
+
+function timeout_callback() {
+  alert("早急に入力してください。在庫がなくなってしまいます。");
 }
+setTimeout(timeout_callback,240000);
